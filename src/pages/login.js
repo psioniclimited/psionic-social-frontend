7c6454802b59bb31a8af1ce59f23262a31bd2b04
sabbir-anwar <protik26@gmail.com>
2020-10-18 10:38:40 +0600

    project init

---
+++ src/pages/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class login extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                <h1>Login Page</h1>
+            </div>
+        );
+    }
+}
+ 
+export default login;
